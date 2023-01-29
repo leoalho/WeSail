e1200a30fb2171fd188b18d16ba02f9e110d76d9
Leo <leo.a.alho@gmail.com>
2023-01-30 00:59:17 +0200

    linting changes

--- wesailfrontend/src/components/SingleBoat/index.tsx
+++ wesailfrontend/src/components/SingleBoat/index.tsx
@@ -3,16 +3,16 @@ import { useState, useEffect } from "react"
 import { deleteFollower, getBoat, updateBoat } from "../../services/boats"
 import { Boat, RootState } from "../../types"
 import { useParams } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { updateUser } from "../../services/users"
-import getUser from "../../services/user"
-import { updateFollowing } from "../../reducers/userReducer"
+//import getUser from "../../services/user"
+//import { updateFollowing } from "../../reducers/userReducer"
 
 const SingleBoat = () => {
   const [boat, setBoat] = useState<(Boat | null)>(null)
   const { id } = useParams();
-  let user = useSelector((state: RootState) => state.user)
-  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user)
+  //const dispatch = useDispatch()
 
   useEffect(() => {
     getBoat(id).then(boat => setBoat(boat)).catch(e => console.log(e))
@@ -34,20 +34,12 @@ const SingleBoat = () => {
   }
 
   const followBoat = async () => {
-    const newUser = await updateUser(user.id, {boatsFollowing: boat.id})
-    console.log(newUser)
-    const newBoat = await updateBoat(boat.id, {follower: user.id})
-    console.log(newBoat)
-    dispatch(updateFollowing(newUser.boatsFollowing))
-    setBoat(newBoat)
+    await updateUser(user.id, {boatsFollowing: boat.id})
+    await updateBoat(boat.id, {follower: user.id})
   }
 
   const unFollowBoat = async () => {
     await deleteFollower(boat.id, user.id)
-    const newUser = await getUser()
-    const newBoat = await getBoat(id)
-    dispatch(updateFollowing(newUser.boatsFollowing))
-    setBoat(newBoat)
   }
 
   return (
