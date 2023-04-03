e2a65f7320331fc01365ece6f5e1fbdd1e2371f4
Leo <leo.a.alho@gmail.com>
2023-04-03 14:59:17 +0300

    Made forms for editing password and email

--- frontend/src/components/User.tsx
+++ frontend/src/components/User.tsx
@@ -5,13 +5,16 @@ import { RootState } from '../types'
 import { updateUser } from '../services/users'
 import { useDispatch } from 'react-redux'
 import { updateFriends, updateFriendRequests } from '../reducers/userReducer'
+import { useState } from 'react'
  
 const UserInput = () => {
+  const [editPassword, setEditPassword] = useState(false)
+  const [editEmail, setEditEmail] = useState(false)
+
   const user = useSelector((state: RootState) => state.user)
   const dispatch = useDispatch()
 
   const acceptRequest = async (id: string) => {
-    //await updateUser(user.id, {op: "remove", path: "/friendRequests", value: id})
     const newUser = await updateUser(user.id, {op: "add", path: "/friends", value: id})
     dispatch(updateFriends(newUser.friends))
     dispatch(updateFriendRequests(newUser.friendRequests))
@@ -27,23 +30,24 @@ const UserInput = () => {
     dispatch(updateFriends(newUser.friends))
   }
 
-  const style = {
-    backgroundColor: "white",
-    padding: "5px"
-  }
-
   return (
-    <div>
-    <div style={style}>
+    <div className="content">
       username: {user.username}<br/>
-      email: {user.email} <button className="button">change</button><br />
-      <button className="button">Change password</button><br />
+      {editEmail
+        ? <><input></input><button className='button'>Save</button><button className='button' onClick={()=>setEditEmail(!editEmail)}>Cancel</button></>
+        : <>email: {user.email} <button className="button" onClick={()=>setEditEmail(!editEmail)}>change</button></>}
+      <br />
+      {editPassword
+        ? <><input></input><br />
+          <input></input><button className='button'>Send</button><button className='button' onClick={()=>setEditPassword(!editPassword)}>Cancel</button></>
+        : <><button className="button" onClick={()=>setEditPassword(!editPassword)}>Change password</button></>}
+      <br />
+
       friend requests:<br />
       {user.friendRequests.map(friend => <div key={friend.id as React.Key}> {friend.username} <button onClick={() => acceptRequest(friend.id)}>Approve</button> <button onClick={()=> declineRequest(friend.id)}>Decline</button> </div>)}
       friends:<br />
       {user.friends.map(friend => <>{friend.username} <button onClick={async () => await deleteFriend(friend.id)}>Remove</button> <br/></>)}
       <Link to="../newBoat">Add boat</Link>
-    </div>
     </div>
   )
 }
