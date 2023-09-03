2b61bbb834c71a80a0a368a69e10cdfdf4e2385e
Leo <leo.a.alho@gmail.com>
2023-09-03 17:13:48 +0300

    Fixe error with linting by eiting craco.config.ts

--- frontend/craco.config.ts
+++ frontend/craco.config.ts
@@ -1,9 +1,19 @@
-import path from 'path';
+import path from "path";
 
 export default {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  eslint: {
+    enable: true /* (default value) */,
+    mode: "extends" /* (default value) */ || "file",
+    configure: (eslintConfig, { env, paths }) => {
+      return eslintConfig;
+    },
+    pluginOptions: (eslintPluginOptions, { env, paths }) => {
+      return eslintPluginOptions;
     },
   },
 };
